8e7df7be9a33b4fbfb2beb339246755616aaca22
washedDeveloper <patrick.brophy2@gmail.com>
2021-04-07 22:22:34 -0400

    fixed route methods

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/updateUser", updateUser);
+app.post("/updateUser", updateUser);
+app.use("/get");
 
 exports.createUser = createUser;
 exports.api = functions.https.onRequest(app);
