55fe70605cdafd0bb510940faebd3006f915edb8
washedDeveloper <patrick.brophy2@gmail.com>
2021-04-07 22:13:08 -0400

    fixed api lol

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,15 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 import createUser from "./createUser";
-import updateUser from "./updateUser";
 import * as express from "express";
 import * as cors from "cors";
+import updateUser from "./updateUser";
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(cors());
-app.post("/updateUser", updateUser);
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.use("/updateUser", updateUser);
 
 exports.createUser = createUser;
 exports.api = functions.https.onRequest(app);
