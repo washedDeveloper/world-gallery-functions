0dd9adefea4c030e16bcf6275e01eb40dd54f45e
washedDeveloper <patrick.brophy2@gmail.com>
2021-04-08 22:56:33 -0400

    changed addPiece to an endpoint

--- src/addPiece.ts
+++ src/addPiece.ts
@@ -1,11 +1,35 @@
-import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin";
+import * as express from "express";
+import * as admin from "firebase-admin";
 
-export const addPiece = functions.storage.object().onFinalize((obj, ctx) => {
-  if (!obj.contentType?.startsWith("images/")) {
-    return false;
+const addPiece = async (req: express.Request, res: express.Response) => {
+  const db = admin.firestore();
+  const userGallery = db.collection("galleries").doc(`${req.body.uid}`);
+  if (userGallery) {
+    try {
+      await userGallery.update({
+        pieces: admin.firestore.FieldValue.arrayUnion({
+          description: req.body.description,
+          imageURL: req.body.imageURL,
+          likes: 0,
+          title: req.body.title,
+          fileName: req.body.fileName,
+        }),
+      });
+      return res.status(200).json({
+        error: false,
+        message: "sucessfully added piece to gallery",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "failed to add piece to gallery",
+      });
+    }
   }
-  functions.logger.info(obj.name);
-  functions.logger.info(obj.bucket);
-  return true;
-});
+  return res.status(404).json({
+    error: true,
+    message: "no user found",
+  });
+};
+
+export default addPiece;
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import createUser from "./createUser";
 import * as express from "express";
 import * as cors from "cors";
 import updateUser from "./updateUser";
-import { addPiece } from "./addPiece";
+import addPiece from "./addPiece";
 import search from "./search";
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.post("/updateUser", updateUser);
 app.get("/search", search);
+app.post("/addPiece", addPiece);
 
 exports.createUser = createUser;
-exports.addPiece = addPiece;
 exports.api = functions.https.onRequest(app);
